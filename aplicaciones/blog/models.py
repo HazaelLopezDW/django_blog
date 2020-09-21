2609a9bfdbc8311e7a8a1cdcecd5a8d7bac356ee
Hazael Lopez <elhazael134@gmail.com>
2020-09-20 23:33:16 -0600

    Arreglamos un pequeño error de escritura en models

--- aplicaciones/blog/models.py
+++ aplicaciones/blog/models.py
@@ -34,9 +34,10 @@ class Autor(models.Model):
 
 class Post(models.Model):
     id = models.AutoField(primary_key = True)
-    titulo = model.CharField('Titulo', max_length = 90, blank = False, null = False)
+    titulo = models.CharField('Titulo', max_length = 90, blank = False, null = False)
     slug = models.CharField('Slug', max_length = 100, blank = False, null = False)
     descripcion = models.CharField('Descripcion', max_length = 100, blank = False, null = False)
+    contenido = models.TextField('Contenido')
     imagen = models.URLField(max_length = 255, blank = False, null = False)
     autor = models.ForeignKey(Autor, on_delete = models.CASCADE)
     categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
