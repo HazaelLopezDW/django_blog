e0b87d5cc935d4fbe9986ebb044c4adf881ed154
Hazael Lopez <elhazael134@gmail.com>
2020-09-20 23:07:47 -0600

    Creamos una nueva clase CategoriaResource(resource.ModelResource): y a nuestra clase CategoriaAdmin le pasamos una nueva herencia ImportExportModelAdmin

--- aplicaciones/blog/admin.py
+++ aplicaciones/blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from .models import *
 from import_export import resources
-from import_export.admin import ImportExportModelAdmin 
+from import_export.admin import ImportExportModelAdmin
 
-class CategoriaAdmin(admin.ModelAdmin):
+class CategoriaResource(resources.ModelResource):
+    class Meta:
+        model = Categoria
+
+class CategoriaAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     search_fields = ['nombre']
     list_display = ('nombre','estado','fecha_creacion',)
+    resources_class = CategoriaResource
 
 class AutorAdmin(admin.ModelAdmin):
     search_fields = ['nombres','apellidos','correo']
