b7e224ee4151214b274ae7616e3f14b6a7ebf398
Hazael Lopez <elhazael134@gmail.com>
2020-09-20 23:02:10 -0600

    Importamos algunas clases de import_export y de import_export_admin

--- aplicaciones/blog/admin.py
+++ aplicaciones/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin 
 
 class CategoriaAdmin(admin.ModelAdmin):
     search_fields = ['nombre']
