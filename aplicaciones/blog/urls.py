859a21802a23e0e07eb9d285b3b7cbd3f31e8e40
Hazael Lopez <elhazael134@gmail.com>
2020-09-21 23:06:07 -0600

    Importamos aluganas imagenes en nuestra archivos static Creamos los templates para nuestro menu de navegacion Creamos nuestras vistas en views.py y creamos las urls en urls.py

--- aplicaciones/blog/urls.py
+++ aplicaciones/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import Home,Generales
+from .views import *
 
 urlpatterns = [
     path('', Home, name = 'index'),
     path('generales/', Generales, name = 'generales'),
+    path('programacion/', Programacion, name = 'programacion'),
+    path('videojuegos/', Videojuegos, name = 'videojuegos'),
+    path('tecnologia/', Tecnologia, name = 'tecnologia'),
+    path('tutoriales/', Tutoriales, name = 'tutoriales'),
 ]
