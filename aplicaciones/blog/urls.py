674936d026481baf3a25b6bcd9835e0457465c6c
Hazael Lopez <elhazael134@gmail.com>
2020-09-21 22:10:39 -0600

    Agregamos una nueva ruta para nuestro template generales

--- aplicaciones/blog/urls.py
+++ aplicaciones/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home,Generales
 
 urlpatterns = [
     path('', Home, name = 'index'),
+    path('', Generales, name = 'generales'),
 ]
