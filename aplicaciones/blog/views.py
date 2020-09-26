5efd5beaa1ee41c80fa3fa1be0d0544e53af47bc
Hazael Lopez <elhazael134@gmail.com>
2020-09-26 11:24:00 -0600

    Modificamos los metodos para hacer las busquedas

--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -33,29 +33,61 @@ def Generales(request):
     return render(request, 'generales.html',{'posts':posts})
 
 def Programacion(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(
         estado = True,
         categoria = Categoria.objects.get(nombre__iexact = 'Programacion')
     )
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains = queryset) |
+            Q(descripcion__icontains = queryset),
+            estado = True,
+            categoria = Categoria.objects.get(nombre__iexact = 'Programacion')
+        ).distinct()
     return render(request, 'programacion.html', {'posts':posts})
 
 def Videojuegos(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(
         estado = True,
         categoria = Categoria.objects.get(nombre__iexact = 'VideoJuegos')
     )
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains = queryset) |
+            Q(descripcion__icontains = queryset),
+            estado = True,
+            categoria = Categoria.objects.get(nombre__iexact = 'VideoJuegos')
+        ).distinct()
     return render(request, 'videojuegos.html', {'posts':posts})
 
 def Tecnologia(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(
         estado = True,
         categoria = Categoria.objects.get(nombre__iexact = 'Tecnologia')
     )
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains = queryset) |
+            Q(descripcion__icontains = queryset),
+            estado = True,
+            categoria = Categoria.objects.get(nombre__iexact = 'Tecnologia')
+        ).distinct()
     return render(request, 'tecnologia.html', {'posts':posts})
 
 def Tutoriales(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(
         estado = True,
         categoria = Categoria.objects.get(nombre__iexact = 'Tutoriales')
     )
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains = queryset) |
+            Q(descripcion__icontains = queryset),
+            estado = True,
+            categoria = Categoria.objects.get(nombre__iexact = 'Tutoriales')
+        ).distinct()
     return render(request, 'tutoriales.html', {'posts':posts})
